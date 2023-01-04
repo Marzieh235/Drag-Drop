e47a5e39385be0f289187916478b6fe667a561be
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-04 15:16:09 +0330

    drag & drop Event

--- draggable.js
+++ draggable.js
@@ -1,6 +1,10 @@
 class draggable {
     constructor(options) {
-        this.setuplist(options)
+        this.setuplist(options);
+
+        for (let listItem of options.el.children) {
+            this.addDnDHandlers(listItem)
+        }
     }
 
 
@@ -9,15 +13,54 @@ class draggable {
         let { list, el: element, template } = options;
 
 
-        if(!element) throw Error('the list is not exist');
-        if(!list) throw Error('the item is not exist');
-        if(!Array.isArray(list)) throw Error('please insert correct array')
-        if(!template) throw Error('please add a template function')
-        if(typeof template !== "function") throw Error('please add a function as template')
+        if (!element) throw Error('the list is not exist');
+        if (!list) throw Error('the item is not exist');
+        if (!Array.isArray(list)) throw Error('please insert correct array')
+        if (!template) throw Error('please add a template function')
+        if (typeof template !== "function") throw Error('please add a function as template')
 
 
         list.forEach(item => element.innerHTML += template(item))
     }
+
+
+    addDnDHandlers(element) {
+        element.setattributes('draggable', true);
+
+
+        element.addEventlistener('dragstart' , this.handlerdragstart)
+        element.addEventlistener('dragenter' , this.handlerdragenter)
+        element.addEventlistener('dragover' , this.handlerdragover)
+        element.addEventlistener('dragleave' , this.handlerdragleave)
+        element.addEventlistener('drop' , this.handlerdrop)
+        element.addEventlistener('dragend' , this.handlerdragend)
+    }
+
+
+    handlerdragstart(e){
+
+    }
+
+    handlerdragenter(e){
+
+    }
+
+    handlerdragover(e){
+
+    }
+
+    handlerdragleave(e){
+
+    }
+
+    handlerdrop(e){
+
+    }
+
+    handlerdragend(e){
+
+    }
+    
 }
 
 
